6cab6a2f8cd7e803519799ca809f7cebfeeab20d
Thomas Claessens <tom@itcl.io>
2022-03-25 10:43:41 +0100

    ...

--- srv/nginx/run.sh
+++ srv/nginx/run.sh
@@ -6,7 +6,7 @@ docker rm -f whise-nginx
 docker build --file=${APP_ROOT}/srv/nginx/Dockerfile  --build-arg environment=${ENVIRONMENT} -t whise/nginx .
 
 docker run   --name whise-nginx \
---link whise-api-php \
+--link whise-fpm \
 -p  8085:80 \
 -v  $APP_ROOT:/usr/share/nginx/html \
 -v  $APP_ROOT/srv/nginx/sites-available/default.conf:/etc/nginx/conf.d/default.conf \
