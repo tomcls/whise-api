a4e11ab4364ab25d4d1bb0cb499908bf8996a3b6
Thomas Claessens <tom@itcl.io>
2022-03-25 10:41:45 +0100

    srv adaption

--- srv/nginx/run.sh
+++ srv/nginx/run.sh
@@ -7,7 +7,7 @@ docker build --file=${APP_ROOT}/srv/nginx/Dockerfile  --build-arg environment=${
 
 docker run   --name whise-nginx \
 --link whise-api-php \
--p  80:80 \
+-p  8085:80 \
 -v  $APP_ROOT:/usr/share/nginx/html \
 -v  $APP_ROOT/srv/nginx/sites-available/default.conf:/etc/nginx/conf.d/default.conf \
 -d whise/nginx
