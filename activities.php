e25f143f335f4fd6e08abff50e3dac09a48dd86d
Thomas Claessens <tom@itcl.io>
2024-08-30 12:53:52 +0200

    ...

--- activities.php
+++ activities.php
@@ -94,7 +94,7 @@ try {
       <div>
         <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Nom d'utilisateur</label>
         <div class="mt-2">
-          <input id="username" name="username" type="username" autocomplete="username" required  value="<?=$username?>" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6">
+          <input id="username" name="username" type="username" autocomplete="username" required  value="<?=$username?>" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6">
         </div>
       </div>
 
