f153f1ba686aff743806227343276cd0792dd887
thomas claessens <thomas.claessens@immovlan.be>
2024-08-30 12:46:31 +0200

    ...

--- srv/fpm/run.sh
+++ srv/fpm/run.sh
@@ -6,5 +6,6 @@ docker rm -f whise-fpm
 docker build --file="./srv/fpm/Dockerfile"  --build-arg environment=${env} -t whise-api/fpm .
 
 docker run --name  whise-fpm \
+     -p 192.168.100.5:9009:9000 \
      -v ${APP_ROOT}:/var/www/html \
      -d --restart always  whise-api/fpm
--- srv/nginx/run.sh
+++ srv/nginx/run.sh
@@ -6,8 +6,7 @@ docker rm -f whise-nginx
 docker build --file=$APP_ROOT/srv/nginx/Dockerfile  --build-arg environment=${ENVIRONMENT} -t whise/nginx .
 
 docker run   --name whise-nginx \
---link whise-fpm \
--p  8085:80 \
+-p  192.168.100.5:8085:80 \
 -v  $APP_ROOT:/usr/share/nginx/html \
 -v  $APP_ROOT/srv/nginx/sites-available/default.conf:/etc/nginx/conf.d/default.conf \
 -d --restart always whise/nginx
